1d2f9b5366cc0db9ac1f4de6731196cbb3da2390
HP <pravinltupe4399@gmail.com>
2022-12-24 08:31:50 +0530

    Adding all sample program

---
+++ Polymorphism/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+  ClassMonitor classMonitor =new ClassMonitor("pravin");
+        System.out.println(classMonitor);
+
+    }
+}
+
